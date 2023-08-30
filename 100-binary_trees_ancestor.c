532783ad3c18de62563b3d771bcfa1b53d473f71
Vanbasten01 <fouad.yasin01@gmail.com>
2023-08-30 18:51:51 +0100

    Advanced Task 1

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ *binary_trees_ancestor -  a function that finds the lowest
+ *common ancestor of two nodes.
+ *@first: one of the nodes to find their lowest common ancestor.
+ *@second:one of the nodes to find their lowest common ancestor.
+ *Return: a pointer to the lowest common ancestor.
+ *
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *fir = (binary_tree_t *)first;
+	binary_tree_t *sec = (binary_tree_t *)second;
+
+	while (fir && sec)
+	{
+		if (fir->parent == sec->parent)
+			return (fir->parent);
+		if (fir == sec->parent)
+			return (fir);
+		if (sec == fir->parent)
+			return (sec);
+		fir = fir->parent;
+		sec = sec->parent;
+	}
+	return (NULL);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 int binary_tree_is_perfect(const binary_tree_t *tree);
