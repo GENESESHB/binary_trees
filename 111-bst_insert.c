85e35de9f77de9140e83571153858471bfb26a0e
Vanbasten01 <fouad.yasin01@gmail.com>
2023-08-31 23:04:49 +0100

    Advanced Task 7 Bst

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
+ *bst_insert - a function that inserts a value in a Binary Search Tree.
+ *@tree:  a double pointer to the root node of the BST to insert the value.
+ *@value: the value to store in the node to be inserted.
+ *Return: a pointer to the created node, or NULL on failure.
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
@@ -30,7 +31,7 @@ bst_t *bst_insert(bst_t **tree, int value)
 		}
 		if (temp->n < value)
 		{
-			if(!temp->right)
+			if (!temp->right)
 			{
 				temp->right = (bst_t *)binary_tree_node(temp, value);
 				return (temp->right);
@@ -40,10 +41,3 @@ bst_t *bst_insert(bst_t **tree, int value)
 	}
 	return (NULL);
 }
-
-
-
-
-
-
-
