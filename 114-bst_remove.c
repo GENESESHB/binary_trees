122ee19e75f2f88d760d9d73d522cc4dfdd16ece
Vanbasten01 <fouad.yasin01@gmail.com>
2023-09-01 02:19:52 +0100

    Advanced Task 10 Bst

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,54 +1,59 @@
 #include "binary_trees.h"
+#include "binary_trees.h"
+
 /**
- *bst_remove -  a function that removes a node from a BST.
- *@root: a pointer to the root node of the BST.
- *@value: the value to be removed.
- *Return:  a pointer to the new root node of the tree after
- *removing the desired value.
+ * bst_remove -  a function that removes a node from a BST.
+ * @root: a pointer to the root node of the BST.
+ * @value: the value to be removed.
+ * Return: a pointer to the new root node of the tree after
+ * removing the desired value.
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
 	bst_t *temp = NULL;
 
 	if (!root)
-		return (NULL);
+		return NULL;
 
-	if (value < root->n)
-		root->left = bst_remove(root->left, value);
-	else if (value > root->n)
-		root->right = bst_remove(root->right, value);
-	else
+	if (value == root->n)
 	{
 		if (!root->left)
 		{
 			temp = root->right;
 			free(root);
-			return (temp);
+			return temp;
 		}
 		else if (!root->right)
 		{
 			temp = root->left;
 			free(root);
-			return (temp);
+			return temp;
 		}
+
 		temp = bst_find_min(root->right);
 		root->n = temp->n;
 		root->right = bst_remove(root->right, temp->n);
+		return root;
 	}
-	return (root);
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else
+		root->right = bst_remove(root->right, value);
+
+	return root;
 }
 
 /**
- *bst_find_min - a function that helps to find the minimum node in a BST.
- *@node: a pointer to the root node of the subtree to search.
- *Return: a pointer to the minimum node in the subtree.
+ * bst_find_min - a function that helps to find the minimum node in a BST.
+ * @node: a pointer to the root node of the subtree to search.
+ * Return: a pointer to the minimum node in the subtree.
  */
 bst_t *bst_find_min(bst_t *node)
 {
 	while (node->left)
 		node = node->left;
 
-	return (node);
+	return node;
 }
-
 
