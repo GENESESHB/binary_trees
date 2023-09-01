9b6492fb29efd45a393f256488af886fb0a69444
Vanbasten01 <fouad.yasin01@gmail.com>
2023-09-01 01:36:56 +0100

    Advanced Task 9 Bst

--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,9 @@ typedef struct binary_tree_s
 } binary_tree_t;
 typedef struct binary_tree_s bst_t;
 
+
+bst_t *bst_find_min(bst_t *node);
+bst_t *bst_remove(bst_t *root, int value);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_insert(bst_t **tree, int value);
